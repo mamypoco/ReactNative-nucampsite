fd10c35f459cb3dd5c4b099cb8d82d97196bdc84
Mami Nishiwaki <mamypoco@gmail.com>
2022-05-24 12:16:26 -0700

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,80 +1,88 @@
-import React, { Component } from 'react';
-import { Text, ScrollView, FlatList } from 'react-native';
-import { Card, ListItem} from 'react-native-elements';
-import { connect } from 'react-redux';
-import { baseUrl } from '../shared/baseUrl';
-import Loading from './LoadingComponent';
+import React, { Component } from "react";
+import { Text, ScrollView, FlatList } from "react-native";
+import { Card, ListItem } from "react-native-elements";
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
+import * as Animatable from "react-native-animatable";
 
-const mapStateToProps = state => {
-    return {
-        partners: state.partners
-    };   
+const mapStateToProps = (state) => {
+  return {
+    partners: state.partners,
+  };
 };
 
 class About extends Component {
+  static navigationOptions = {
+    title: "About Us",
+  };
+  render() {
+    const renderPartner = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          leftAvatar={{ source: { uri: baseUrl + item.image } }}
+        />
+      );
+    };
 
-    static navigationOptions = {
-        title: 'About Us'
+    if (this.props.partners.isLoading) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Loading />
+          </Card>
+        </ScrollView>
+      );
     }
-    render() {
-        const renderPartner = ({item}) => {
-            return (
-            <ListItem
-                title={item.name}
-                subtitle={item.description}
-                leftAvatar={{ source: {uri: baseUrl + item.image}}}
-                />
-            )
-        }
-
-        if (this.props.partners.isLoading) {
-            return (
-                <ScrollView>
-                    <Mission />
-                    <Card 
-                        title="Community Partners">
-                        <Loading />    
-                    </Card>
-                </ScrollView>
-            );
-        }
-        if (this.props.partners.errMess) {
-            return (
-                <ScrollView>
-                    <Mission />
-                    <Card 
-                        title="Community Partners">
-                        <Text>{this.props.partners.errMess}</Text> 
-                    </Card>
-                </ScrollView>
-            );
-        } 
-
-        return (
-            <ScrollView>
-                 <Mission />
-                 <Card title="Community Partners">
-                 <FlatList
-                    data={this.props.partners.partners}
-                    //first partners refers to the entire part of the state that handles partners data including isLoading & error message properties along with the partners array. The 2nd one refers to the parner's data array.//
-                    renderItem={renderPartner}
-                    keyExtractor={item => item.id.toString()}
-                    />
-                 </Card>
-            </ScrollView>
-        );            
+    if (this.props.partners.errMess) {
+      return (
+        <ScrollView>
+          <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+            <Mission />
+            <Card title="Community Partners">
+              <Text>{this.props.partners.errMess}</Text>
+            </Card>
+          </Animatable.View>
+        </ScrollView>
+      );
     }
+
+    return (
+      <ScrollView>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Mission />
+          <Card title="Community Partners">
+            <FlatList
+              data={this.props.partners.partners}
+              //first partners refers to the entire part of the state that handles partners data including isLoading & error message properties along with the partners array. The 2nd one refers to the parner's data array.//
+              renderItem={renderPartner}
+              keyExtractor={(item) => item.id.toString()}
+            />
+          </Card>
+        </Animatable.View>
+      </ScrollView>
+    );
+  }
 }
 
 const Mission = () => {
-    return (
+  return (
     <Card title="Our Mission">
-        <Text style={{margin: 10}}>
-            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
-        </Text>
+      <Text style={{ margin: 10 }}>
+        We present a curated database of the best campsites in the vast woods
+        and backcountry of the World Wide Web Wilderness. We increase access to
+        adventure for the public while promoting safe and respectful use of
+        resources. The expert wilderness trekkers on our staff personally verify
+        each campsite to make sure that they are up to our standards. We also
+        present a platform for campers to share reviews on campsites they have
+        visited with each other.
+      </Text>
     </Card>
-    )
-}
+  );
+};
 
 export default connect(mapStateToProps)(About);
-//It was just About. Now we need connect this component to redux store. We need to pass mapStateProps function that we created earlier. About component now receives the partners props from the redux store.
+//Now we need connect this component to redux store. We need to pass mapStateProps function that we created earlier. About component now receives the partners props from the redux store.
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import { Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
+import React, { Component } from "react";
+import { Text, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
+import * as Animatable from "react-native-animatable";
 
 class Contact extends Component {
-
-    static navigationOptions = {
-        title: 'Contact Us'
-    }
-    render() {
-        return (
-            <ScrollView>
-                <Card title="Contact Information" wrapperStyle={{margin: 20}}>
-                    <Text style={{marginBottom: 10}}>
-                            1 Nucamp Way{"\n"}
-                            Seattle, WA 98001  {"\n"}
-                            U.S.A.  {"\n"}
-                            {"\n"}
-                            Phone: 1-206-555-1234{"\n"}
-                            Email: campsites@nucamp.co
-                    </Text>
-                </Card>
-            </ScrollView>
-        )     
-    }
+  static navigationOptions = {
+    title: "Contact Us",
+  };
+  render() {
+    return (
+      <ScrollView>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
+            <Text style={{ marginBottom: 10 }}>
+              1 Nucamp Way{"\n"}
+              Seattle, WA 98001 {"\n"}
+              U.S.A. {"\n"}
+              {"\n"}
+              Phone: 1-206-555-1234{"\n"}
+              Email: campsites@nucamp.co
+            </Text>
+          </Card>
+        </Animatable.View>
+      </ScrollView>
+    );
+  }
 }
 
 export default Contact;
